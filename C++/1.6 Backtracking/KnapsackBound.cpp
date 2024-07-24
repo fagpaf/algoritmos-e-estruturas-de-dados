d9d888b270f3c4a431024edacc4609876ef053e9
fagpaf <fagpaf@cin.ufpe.br>
2024-07-24 11:12:55 -0300

    ad

--- C++/1.6 Backtracking/KnapsackBound.cpp
+++ C++/1.6 Backtracking/KnapsackBound.cpp
@@ -1,1 +1,48 @@
-int ub = v + (W - w) * (v[i+1] / w[i+1]); // Upper Bound
+// int ub = v + (W - w) * (v[i+1] / w[i+1]); // Upper Bound
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int max(int l, int r);
+int knapsack(int n, int W, int w[], int v[], int F[n][W]);
+
+int main() {
+    
+    int n = 4; // Número de itens
+    int W = 7; // Capacidade da mochila
+    int w[] = {1, 3, 4, 5}; // Pesos dos itens
+    int v[] = {1, 4, 5, 7}; // Valores dos itens
+    int F[1001][1001]; // Matriz de programação dinâmica (com um tamanho grande para evitar problemas de limite)
+
+    int maxVal = knapsack(n, W, w, v, F);
+    cout << "O valor máximo que pode ser colocado na mochila é " << maxVal << endl;
+    
+    return 0;
+}
+
+int max(int l, int r){
+    if(l > r){
+        return l;
+    }
+    else{
+        return r;
+    }
+}
+
+int knapsack(int n, int W, int w[], int v[], int F[n][W]){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < W; j++){
+            if(i == 0 || j == 0){
+                F[i][j] = 0;
+            }
+            else if(w[i] <= j){
+                F[i][j] = max(F[i - 1][j], v[i] + F[i - 1][j - w[i]]);
+            }
+            else{
+                F[i][j] = F[i - 1][j];
+            }
+        }
+    }
+    return F[n][W];
+}
--- teste.cpp
+++ teste.cpp
@@ -1,11 +1,43 @@
-#include "Lybrary/Graph.h"
+// #include "Lybrary/Graph.h"
 
+// using namespace std;
+
+// int main() {
+    
+
+    
+//     return 0;
+// }
+// g++ teste.cpp -o teste.exe ; Get-Content input.txt | ./teste.exe
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main() {
-    
+int knapsack(int n, int W, int w[], int v[]) {
+    vector<vector<int>> F(n + 1, vector<int>(W + 1, 0));
 
-    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= W; j++) {
+            if (w[i - 1] <= j) {
+                F[i][j] = max(F[i - 1][j], v[i - 1] + F[i - 1][j - w[i - 1]]);
+            } else {
+                F[i][j] = F[i - 1][j];
+            }
+        }
+    }
+    return F[n][W];
+}
+
+int main() {
+    int n = 4; // Número de itens
+    int W = 7; // Capacidade da mochila
+    int w[] = {1, 3, 4, 5}; // Pesos dos itens
+    int v[] = {1, 4, 5, 7}; // Valores dos itens
+
+    int maxVal = knapsack(n, W, w, v);
+    cout << "O valor maximo que pode ser colocado na mochila e: " << maxVal << endl;
+
     return 0;
 }
-// g++ teste.cpp -o teste.exe ; Get-Content input.txt | ./teste.exe
